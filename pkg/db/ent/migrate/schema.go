f947a8d174d095cae7f4dbec5bd64805067ccf9c
JobNing <q178149@163.com>
2023-02-28 15:40:45 +0800

    update commit

--- pkg/db/ent/migrate/schema.go
+++ pkg/db/ent/migrate/schema.go
@@ -36,7 +36,7 @@ var (
 		{Name: "display_names", Type: field.TypeJSON, Nullable: true},
 		{Name: "enable_purchase", Type: field.TypeBool, Nullable: true, Default: true},
 		{Name: "enable_product_page", Type: field.TypeBool, Nullable: true, Default: true},
-		{Name: "cancel_mode", Type: field.TypeString, Nullable: true, Default: "Uncalcellable"},
+		{Name: "cancel_mode", Type: field.TypeString, Nullable: true, Default: "Uncancellable"},
 		{Name: "user_purchase_limit", Type: field.TypeOther, Nullable: true, SchemaType: map[string]string{"mysql": "decimal(37,18)"}},
 	}
 	// AppGoodsTable holds the schema information for the "app_goods" table.
