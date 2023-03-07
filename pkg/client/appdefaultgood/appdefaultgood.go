437802b902e646286679856dc4615e51f0a17559
JobNing <q178149@163.com>
2023-03-07 15:11:07 +0800

    add default good client

---
+++ pkg/client/appdefaultgood/appdefaultgood.go
@@ -0,0 +1,182 @@
+//nolint:nolintlint,dupl
+package appdefaultgood
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	npool "github.com/NpoolPlatform/message/npool/good/mgr/v1/appdefaultgood"
+
+	constant "github.com/NpoolPlatform/good-manager/pkg/message/const"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.ManagerClient) (cruder.Any, error)
+
+func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewManagerClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func CreateAppDefaultGood(ctx context.Context, in *npool.AppDefaultGoodReq) (*npool.AppDefaultGood, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.CreateAppDefaultGood(ctx, &npool.CreateAppDefaultGoodRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.AppDefaultGood), nil
+}
+
+func CreateAppDefaultGoods(ctx context.Context, in []*npool.AppDefaultGoodReq) ([]*npool.AppDefaultGood, error) {
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.CreateAppDefaultGoods(ctx, &npool.CreateAppDefaultGoodsRequest{
+			Infos: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return infos.([]*npool.AppDefaultGood), nil
+}
+
+func UpdateAppDefaultGood(ctx context.Context, in *npool.AppDefaultGoodReq) (*npool.AppDefaultGood, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.UpdateAppDefaultGood(ctx, &npool.UpdateAppDefaultGoodRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.AppDefaultGood), nil
+}
+
+func GetAppDefaultGood(ctx context.Context, id string) (*npool.AppDefaultGood, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.GetAppDefaultGood(ctx, &npool.GetAppDefaultGoodRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.AppDefaultGood), nil
+}
+
+func GetAppDefaultGoodOnly(ctx context.Context, conds *npool.Conds) (*npool.AppDefaultGood, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.GetAppDefaultGoodOnly(ctx, &npool.GetAppDefaultGoodOnlyRequest{
+			Conds: conds,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.AppDefaultGood), nil
+}
+
+func GetAppDefaultGoods(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.AppDefaultGood, uint32, error) {
+	var total uint32
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.GetAppDefaultGoods(ctx, &npool.GetAppDefaultGoodsRequest{
+			Conds:  conds,
+			Limit:  limit,
+			Offset: offset,
+		})
+		if err != nil {
+			return nil, err
+		}
+		total = resp.GetTotal()
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*npool.AppDefaultGood), total, nil
+}
+
+func ExistAppDefaultGood(ctx context.Context, id string) (bool, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.ExistAppDefaultGood(ctx, &npool.ExistAppDefaultGoodRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return false, err
+	}
+	return info.(bool), nil
+}
+
+func ExistAppDefaultGoodConds(ctx context.Context, conds *npool.Conds) (bool, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.ExistAppDefaultGoodConds(ctx, &npool.ExistAppDefaultGoodCondsRequest{
+			Conds: conds,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return false, err
+	}
+	return info.(bool), nil
+}
+
+func DeleteAppDefaultGood(ctx context.Context, id string) (*npool.AppDefaultGood, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
+		resp, err := cli.DeleteAppDefaultGood(ctx, &npool.DeleteAppDefaultGoodRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.AppDefaultGood), nil
+}
